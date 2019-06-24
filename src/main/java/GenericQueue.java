79eaeb9875fe56c74fa12b4c77454310734dedd2
Seweryn91 <s.talaj@gmail.com>
2019-06-24 15:36:32 +0200

    Implement method insertNode

--- src/main/java/GenericQueue.java
+++ src/main/java/GenericQueue.java
@@ -35,4 +35,28 @@ public class GenericQueue<T> {
     private Node head;
     private Node tail;
     private int size = 0;
+
+    public void enqueue(T element) {
+        //TODO: implement method enqueue with priority
+    }
+
+    private void insertNode(Node node) {
+        Node currentNode = this.head;
+
+        if (currentNode.getPriority() < node.getPriority()) {
+            node.setNextNode(currentNode);
+            this.head = node;
+        } else {
+            Node nextNode = currentNode.getNextNode();
+            while (nextNode != null && nextNode.getPriority() >= node.getPriority()) {
+                currentNode = currentNode.getNextNode();
+                nextNode = nextNode.getNextNode();
+            }
+            currentNode.setNextNode(node);
+            node.setNextNode(nextNode);
+
+            if (nextNode == null) {
+                this.tail = node;
+            }
+        }
 }
