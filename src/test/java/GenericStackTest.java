05aba250e659c3c7c4e370cbe90c37b5023b4867
Seweryn91 <s.talaj@gmail.com>
2019-06-24 15:20:12 +0200

    Create method testPush

--- src/test/java/GenericStackTest.java
+++ src/test/java/GenericStackTest.java
@@ -1,6 +1,9 @@
+import exceptions.StackOverflowException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class GenericStackTest {
@@ -15,6 +18,20 @@ public class GenericStackTest {
     @DisplayName("Test initializing with wrong arument: -5")
     void testInitialization_wrongArgument_minus5() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new GenericStack<Integer>(-5));
+    }
+
+    @Test
+    @DisplayName("Test pushing elements")
+    @SuppressWarnings("unchecked")
+    void testPush() throws StackOverflowException {
+        GenericStack stack = new GenericStack(10);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        int expectedSize = 3;
+        int actualSize = stack.size();
+        Assertions.assertEquals(expectedSize, actualSize);
     }
 
 }
