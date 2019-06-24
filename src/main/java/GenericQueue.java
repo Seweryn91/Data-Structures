8ea7f0beb9f18f716cbdd8a4483210d6756267f1
Seweryn91 <s.talaj@gmail.com>
2019-06-24 15:33:37 +0200

    Implement inner class Node

--- src/main/java/GenericQueue.java
+++ src/main/java/GenericQueue.java
@@ -1,4 +1,36 @@
 public class GenericQueue<T> {
 
+    class Node {
+        private T value;
+        private Node nextNode;
+        private Integer priority = -1;
+
+        public Node(T value){
+            this.value = value;
+        }
+
+        public Node(T value, Integer priority) {
+            this.value = value;
+            this.priority = priority;
+        }
+
+        public T getValue() {
+            return this.value;
+        }
+
+        public Node getNextNode() {
+            return this.nextNode;
+        }
+
+        public Integer getPriority() {
+            return priority;
+        }
+
+        public void setNextNode(Node node) {
+            this.nextNode = node;
+        }
+
+    }
+
 
 }
