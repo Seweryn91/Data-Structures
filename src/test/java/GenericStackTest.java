ead077b71fa67e6d5f2fa6e4b22ffa0f3c776433
Seweryn91 <s.talaj@gmail.com>
2019-06-24 15:18:28 +0200

    Implement method testInitialization_wrongArgument_minus5

--- src/test/java/GenericStackTest.java
+++ src/test/java/GenericStackTest.java
@@ -6,9 +6,15 @@ import org.junit.jupiter.api.Test;
 public class GenericStackTest {
 
     @Test
-    @DisplayName("Test initializing with wrong argument")
+    @DisplayName("Test initializing with wrong argument: 0")
     void testInitialization_wrongArgument_0() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new GenericStack<Integer>(0));
+    }
+
+    @Test
+    @DisplayName("Test initializing with wrong arument: -5")
+    void testInitialization_wrongArgument_minus5() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new GenericStack<Integer>(-5));
     }
 
 }
