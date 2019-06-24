61ac5a6fe6107ad9be2823ee75bf29609a378348
Seweryn91 <s.talaj@gmail.com>
2019-06-24 16:02:42 +0200

    Create inner class Node

--- src/main/java/GenericSinglyLinkedList.java
+++ src/main/java/GenericSinglyLinkedList.java
@@ -1,2 +1,28 @@
 public class GenericSinglyLinkedList<T> {
+
+    class Node<T>{
+        private T content;
+        private Node next;
+
+        Node(T content) {
+            this.content = content;
+        }
+
+        public Node next() {
+            return this.next;
+        }
+
+        public T getContent() {
+            return this.content;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return content.toString();
+        }
+    }
 }
