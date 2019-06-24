127ec0ba0aa1e53ff12a8f50a2fda4c597bd405a
Seweryn91 <s.talaj@gmail.com>
2019-06-24 15:47:30 +0200

    Implement method createQueue

--- src/test/java/GenericQueueTest.java
+++ src/test/java/GenericQueueTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenericQueueTest {
+
+    @SuppressWarnings("unchecked")
+    private GenericQueue createQueue(int size) {
+        GenericQueue queue = new GenericQueue();
+
+        for (int i = 0; i < size; i++) {
+            queue.enqueue(i);
+        }
+    return queue;
+    }
 
 }
