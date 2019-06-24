20eb158011447ae4503c02fa740380c62972fd5b
Seweryn91 <s.talaj@gmail.com>
2019-06-24 15:48:23 +0200

    Implement method enqueueTest

--- src/test/java/GenericQueueTest.java
+++ src/test/java/GenericQueueTest.java
@@ -5,6 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GenericQueueTest {
 
+    @Test
+    @DisplayName("Test enqueue without priority")
+    void enqueueTest() {
+        int expectedSize = 2;
+        GenericQueue queue = createQueue(expectedSize);
+        assertEquals(expectedSize, queue.size());
+    }
+
+    @Test
+    @DisplayName("Test enqueue with priority")
+    void enqueueTest_withPriority() {
+        GenericQueue<String> queue = new GenericQueue<>();
+        queue.enqueue("Johann", 42);
+        queue.enqueue("Bach", 1);
+        queue.enqueue("Sebastian", 10);
+
+        String expectedResult = "Johann";
+        String actualResult = queue.peek();
+        assertEquals(expectedResult, actualResult);
+    }
+
+
     @SuppressWarnings("unchecked")
     private GenericQueue createQueue(int size) {
         GenericQueue queue = new GenericQueue();
