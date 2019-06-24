6cd4dc17e70a339f74622cba8bae9707d07a807f
Seweryn91 <s.talaj@gmail.com>
2019-06-24 15:54:30 +0200

    Change import of junit.jupiter.api.Assertions.assertThrows to static

--- src/test/java/GenericStackTest.java
+++ src/test/java/GenericStackTest.java
@@ -1,10 +1,10 @@
 import exceptions.StackOverflowException;
 import exceptions.StackUnderflowException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class GenericStackTest {
@@ -12,13 +12,13 @@ public class GenericStackTest {
     @Test
     @DisplayName("Test initializing with wrong argument: 0")
     void testInitialization_wrongArgument_0() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new GenericStack<Integer>(0));
+        assertThrows(IllegalArgumentException.class, () -> new GenericStack<Integer>(0));
     }
 
     @Test
     @DisplayName("Test initializing with wrong arument: -5")
     void testInitialization_wrongArgument_minus5() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new GenericStack<Integer>(-5));
+        assertThrows(IllegalArgumentException.class, () -> new GenericStack<Integer>(-5));
     }
 
     @Test
@@ -41,7 +41,7 @@ public class GenericStackTest {
     void testStackOverflow() throws StackOverflowException {
         GenericStack stack = new GenericStack(1);
         stack.push(1);
-        Assertions.assertThrows(StackOverflowException.class, () -> stack.push(2));
+        assertThrows(StackOverflowException.class, () -> stack.push(2));
     }
 
     @Test
@@ -51,7 +51,7 @@ public class GenericStackTest {
         GenericStack stack = new GenericStack(1);
         stack.push(1);
         stack.pop();
-        Assertions.assertThrows(StackUnderflowException.class, () -> stack.pop());
+        assertThrows(StackUnderflowException.class, () -> stack.pop());
     }
 
     @Test
