d0b95c172ab406662656f9c0fcb439c2c3346f67
Seweryn91 <s.talaj@gmail.com>
2019-06-24 16:47:27 +0200

    Create inner class Node

--- src/main/java/GenericDoublyLinkedList.java
+++ src/main/java/GenericDoublyLinkedList.java
@@ -1,2 +1,28 @@
-public class GenericDoublyLinkedList {
+@SuppressWarnings("unchecked")
+public class GenericDoublyLinkedList<T> {
+
+    class Node <T>{
+        private T content;
+        private Node previous;
+        private Node next;
+
+        Node(T content) { this.content = content; }
+
+        public Node previous() { return this.previous; }
+
+        public Node next() { return this.next; }
+
+        public T getContent() { return this.content; }
+
+        public void setPrevious(Node previous) { this.previous = previous; }
+
+        public void setNext(Node next) { this.next = next; }
+
+        @Override
+        public String toString() { return content.toString(); }
+
+    }
+
+
+
 }
