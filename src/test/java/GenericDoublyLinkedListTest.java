cd77ce3bf7382fec68184526415f158652931c48
Seweryn91 <s.talaj@gmail.com>
2019-06-24 17:02:24 +0200

    Implement method createList

--- src/test/java/GenericDoublyLinkedListTest.java
+++ src/test/java/GenericDoublyLinkedListTest.java
@@ -2,4 +2,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GenericDoublyLinkedListTest {
 
+
+    @SuppressWarnings("unchecked")
+    private GenericDoublyLinkedList createList(int size){
+        GenericDoublyLinkedList list = new GenericDoublyLinkedList();
+        for (int i=0; i < size; i++)
+            list.add(i);
+
+        return list;
+    }
+
 }
