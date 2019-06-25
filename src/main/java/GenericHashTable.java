b1425b7d5a8e8ddf9b89655770a94967add5285b
Seweryn91 <s.talaj@gmail.com>
2019-06-25 18:06:36 +0200

    Implement inner class KeyValue

--- src/main/java/GenericHashTable.java
+++ src/main/java/GenericHashTable.java
@@ -1,2 +1,28 @@
 public class GenericHashTable {
+    class KeyValue<K, V> {
+        public K key;
+
+        public V value;
+
+        public KeyValue (K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        K getKey() {
+            return key;
+        }
+
+        V getValue() {
+            return value;
+        }
+
+        void setKey(K key) {
+            this.key = key;
+        }
+
+        void setValue(V value) {
+            this.value = value;
+        }
+    }
 }
