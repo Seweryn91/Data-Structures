117a1185a04f4ee7223f607829a1443f32aa5845
Seweryn91 <s.talaj@gmail.com>
2019-06-25 18:22:41 +0200

    Implement method testAdd

--- src/test/java/GenericHashTableTest.java
+++ src/test/java/GenericHashTableTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenericHashTableTest {
+
+    @Test
+    @DisplayName("Test add")
+    void testAdd() {
+        GenericHashTable<String, Integer> table = new GenericHashTable<>();
+        table.add("Five", 5);
+        assertEquals(1, table.size());
+    }
 
 }
