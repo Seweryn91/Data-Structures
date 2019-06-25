3b8924b6e454ac015be46727de6a79da6d06239e
Seweryn91 <s.talaj@gmail.com>
2019-06-25 18:39:05 +0200

    Implement fields and constructors

--- src/main/java/GenericArrayList.java
+++ src/main/java/GenericArrayList.java
@@ -1,2 +1,19 @@
-public class GenericArrayList {
+public class GenericArrayList{
+
+    private int size = 0;
+    private Object[] data;
+    private int capacity;
+
+    public GenericArrayList(){
+        data = new Object[1];
+        size = 0;
+    }
+
+    public GenericArrayList(int capacity){
+        if (capacity < 1){
+            capacity = 1;
+        }
+        data = new Object[capacity];
+        size = 0;
+    }
 }
